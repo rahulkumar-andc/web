511250f175ffc2a8c04db039ebd633117d192b73
villenbhai <pk6299160241@gmail.com>
2025-06-04 02:12:11 +0530

    config_db_in_setting.py

--- villen/settings.py
+++ villen/settings.py
@@ -85,11 +85,13 @@ WSGI_APPLICATION = 'villen.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
 
 
